10a27b0e9dad949fd0bf2b601243e2b30873888b
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-20 21:12:14 +0530

    Update userController.java

--- Project/Controller/userController.java
+++ Project/Controller/userController.java
@@ -8,3 +8,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+public class UserController {
+
+    UserService userservice;
+
+    public UserController(UserService userservice) {
+        this.userservice = userservice;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserClassSpringBoot> createUser(@RequestBody UserClassSpringBoot user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userservice.create(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserClassSpringBoot>> getUser() {
+         return ResponseEntity.status(HttpStatus.ACCEPTED).body(userservice.getAllUsers());
+    }
+}
