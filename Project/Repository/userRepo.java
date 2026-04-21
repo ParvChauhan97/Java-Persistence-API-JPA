d0afab011d15d7740222a8545c6b8734124b71c9
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-21 21:15:47 +0530

    Update userRepo.java

--- Project/Repository/userRepo.java
+++ Project/Repository/userRepo.java
@@ -1,1 +1,9 @@
+package com.example.jpademo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<UserClassSpringBoot, Long> {
+
+}
