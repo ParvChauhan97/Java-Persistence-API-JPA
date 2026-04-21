adb0e35ddd6e80f3d0714e6f8d0dd413e5d6ed73
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-21 21:15:20 +0530

    Update userClassSpringBoot.java

--- Project/Model/userClassSpringBoot.java
+++ Project/Model/userClassSpringBoot.java
@@ -1,1 +1,45 @@
+package com.example.jpademo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserClassSpringBoot {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+
+    UserClassSpringBoot() {
+
+    }
+
+    UserClassSpringBoot(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UserClassSpringBoot(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
