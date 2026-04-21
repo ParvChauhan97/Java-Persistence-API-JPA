5d0451c67ef22ca104c3cf87189df116b2b438dc
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-21 21:16:23 +0530

    Update userService.java

--- Project/Service/userService.java
+++ Project/Service/userService.java
@@ -1,1 +1,20 @@
+package com.example.jpademo;
 
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+    UserRepo userrepo;
+    public UserService(UserRepo userrepo) {
+        this.userrepo = userrepo;
+    }
+
+    public UserClassSpringBoot create(UserClassSpringBoot user) {
+        return userrepo.save(user);
+    }
+
+    public List<UserClassSpringBoot> getAllUsers() {
+        return userrepo.findAll();
+    }
+}
